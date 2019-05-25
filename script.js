be6a67be328ca502583cdcb6eeadb70420ec21f5
kimjuliana <juliana.kg@gmail.com>
2019-05-24 22:24:21 -0300

    metodo delete

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ let foto = document.querySelector(".imagem");
 
 fetch('http://localhost:5001/maravilhosas')
 
-.then((response) =>{
-    return response.json();
-})
-.then((data) => {
-    data.content.forEach(mulher =>{
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        data.content.forEach(mulher => {
 
         let perfil = document.createElement('div');
         perfil.setAttribute('class', 'maravilhosas__perfil');
@@ -27,6 +27,36 @@ fetch('http://localhost:5001/maravilhosas')
         nome.innerHTML = mulher.title || mulher.nome;
         link.appendChild(nome);
         
+            const botaoDeletar = document.createElement('button');
+            botaoDeletar.innerHTML = "x";
+            botaoDeletar.setAttribute('class', 'deletar');
+            botaoDeletar.setAttribute('data-id', mulher.id);
+            perfil.appendChild(botaoDeletar);
+
+            botaoDeletar.addEventListener('click', () => {
+                const thisCard = botaoDeletar.parentElement;
+                const cardPai = thisCard.parentElement;
+                const idMulher = botaoDeletar.getAttribute('data-id');
+
+                fetch(`http://localhost:5001/maravilhosas/${idMulher}`, {
+                        method: 'DELETE',
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        },
+
+                    })
+
+                    .then((response) => {
+                        cardPai.removeChild(thisCard)
+                        // return response.json();
+                    })
+
+                    .catch((erro) => {
+                        console.log(erro);
+                    })
+            })
+            
         if(mulher.metadata){
             if(mulher.metadata.image){
                 if(mulher.metadata.image.url){
@@ -41,71 +71,41 @@ fetch('http://localhost:5001/maravilhosas')
         else{
             return imagem.setAttribute("src", "./img/img-mulher.png")
         };
-        
-    })
-})
-.catch((erro) => {
-    console.log(erro);
-})
-
-
-
-
-const botao = document.querySelector('.botao');
-botao.addEventListener('click', () =>{
-    // location.reload();
-
-    fetch ('http://localhost:5001/maravilhosas', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "title": pessoa.value,
-            "foto": foto.value,
-        
         })
-    
-    })
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) =>{
-        console.log(data);
-    
     })
     .catch((erro) => {
         console.log(erro);
-})
-})
-
-
-const botaoDeletar = document.createElement('button');
-botaoDeletar.innerHTML = "x";
-botao.setAttribute('class', 'deletar');
-botao.setAttribute('data-id', 'mulher.id');
-perfil.appendChild(botaoDeletar);
-
-botaoDeletar.addEventListener('click', () =>{
-    fetch('localhost:5001/maravilhosas', {
-        method: 'DELETE',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "id":botao.getAttribute("data-id")
-            
     })
-})
-.then((response) =>{
-    return response.json();
-})
-.then((data) => {
-    console.log(data);
-})
-.catch((erro) => {
-    console.log(erro);
-})
+
+
+
+
+const botaoDeletar = document.querySelector('.botao');
+botaoDeletar.addEventListener('click', () => {
+
+    fetch('http://localhost:5001/maravilhosas', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "title": pessoa.value,
+                "foto": foto.value,
+
+            })
+
+        })
+        .then((response) => {
+            console.log(response);
+            document.location.reload(true);
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+
+        })
+        .catch((erro) => {
+            console.log(erro);
+        })
 })
